6f253aac2fd12607f734185c05710c2dd2c82bba
Mai <interacsion@protonmail.com>
2026-07-01 14:40:21 +0300

    move version_info.ron to src

--- build.rs
+++ build.rs
@@ -5,8 +5,8 @@ use anyhow::{Context, Result};
 fn main() -> Result<()> {
     let out_dir: PathBuf = env::var_os("OUT_DIR").context("OUT_DIR is not set")?.into();
 
-    println!("cargo::rerun-if-changed=version_info.ron");
-    let version_info = fs::read_to_string("version_info.ron")?;
+    println!("cargo::rerun-if-changed=src/version_info.ron");
+    let version_info = fs::read_to_string("src/version_info.ron")?;
     let version_info = mazecode_codegen::generate(&ron::from_str(&version_info)?);
 
     fs::write(
